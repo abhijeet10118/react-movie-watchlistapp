d2e6ef5ed60f963f2f18fc4b9fa69557791b5b48
abhijeet10118 <abhijeet3390@gmail.com>
2025-05-26 00:22:59 +0530

    Initial commit: React movie watchlist app setup

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "5106b3d7cf85a767f91fb3bae50c5e27"
+const BASE_URL="https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () =>{
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async (query) =>{
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    return data.results
+};
